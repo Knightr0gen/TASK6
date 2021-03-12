e0aa17a6d3f1913ed6e728c08efb7da3a50d328a
KARTHIK ANIL <karthik030700@gmail.com>
2021-03-12 14:08:24 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormGroup,FormControl} from '@angular/forms';
+import {Validators} from  '@angular/forms';
+import {FormBuilder} from '@angular/forms';
+import {FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task6';
+      title = 'task6';
+      constructor(private fb: FormBuilder){}
+      user = this.fb.group ({
+        firstname: ['',Validators.required],
+        lastname:  [''],
+        age:  [''],
+        sex:  [''],
+        email:  [''],
+        address:this.fb.group({
+          address1: ['',Validators.required],
+          address2: [''],
+          pin: [''],
+        }),
+        mobile:this.fb.array([
+            this.fb.control(''),
+        ]),
+      });
+
+      onSubmit(){
+        console.log(this.user.value);
+        alert(this.user.value);
+      }
+
+      get mobiles(){
+            return this.user.get('mobile') as FormArray;
+      }
+    addbtn(){
+      this.mobiles.push(this.fb.control(''));
+    }
+    remove(i: number) {
+      this.mobiles.removeAt(i);
+    }
+
 }
